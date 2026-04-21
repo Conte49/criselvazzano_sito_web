04a0dc29c59eed497027c85e0ce8826511972723
Nicola Contin <nicolacontin11@gmail.com>
2026-04-21 10:12:45 +0200

    fix

--- frontend/src/views/Trasparenza.vue
+++ frontend/src/views/Trasparenza.vue
@@ -79,8 +79,8 @@ export default {
   data() {
     return {
       bilanciEsercizio: [
-        { nome: 'Bilancio 2025', anno: '2025', file: 'Bilancio 2025.pdf' },
-        { nome: 'Bilancio 2024', anno: '2024', file: 'Bilancio 2024.pdf' },
+        { nome: 'Bilancio di esercizio 2025', anno: '2025', file: 'Bilancio 2025.pdf' },
+        { nome: 'Bilancio di esercizio 2024', anno: '2024', file: 'Bilancio 2024.pdf' },
       ],
       bilanciPrevisione: [
         { nome: 'Bilancio Preventivo 2026', anno: '2026', file: 'Bilancio Preventivo 2026.pdf' },
